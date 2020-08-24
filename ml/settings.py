01a23e5600fc17ed64d0ab4b0cf59fc09fc09181
Manuel Rodríguez Gómez <33105303+caldefu@users.noreply.github.com>
2020-08-24 12:15:07 +0200

    Update settings.py

--- ml/settings.py
+++ ml/settings.py
@@ -137,4 +137,4 @@ EMAIL_HOST="smtp.gmail.com"
 EMAIL_USE_TLS=True
 EMAIL_PORT=587
 EMAIL_HOST_USER="caldefu@gmail.com"
-EMAIL_HOST_PASSWORD="hmqbpwvbljlutdap"
+EMAIL_HOST_PASSWORD="hmqbpwvbljlutda"
